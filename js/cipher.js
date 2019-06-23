916f80f9e8bc18d2555a1442c97a635683b35d6b
shegsteham <oluwasegunadepoju@gmail.com>
2019-06-23 12:05:02 +0200

    chore(es6): adopt es6 syntax [finishes]

--- js/cipher.js
+++ js/cipher.js
@@ -1,52 +1,50 @@
-/**window.onload=function(){
-var Encrypt = document.getElementById("submit1");
-var Decrypt = document.getElementById("submit2");
-Encrypt.addEventListener("click", encrypt());
-Encrypt.addEventListener("click", decrypt());
-}
-**/
-function encrypt() {
-    var plaintext = document.getElementById("input").value;
-    if ((plaintext.length < 1)) {
-        alert("Please Enter Atleast a Word to Create a Cipher Encryption");
-        return;
+const encryptButton = document.getElementById('encrypt');
+const decryptButton = document.getElementById('decrypt');
+encryptButton.addEventListener('click', () => encrypt());
+decryptButton.addEventListener('click', () => decrypt());
+
+encrypt = () => {
+  const plaintext = document.getElementById("input").value;
+  if ((plaintext.length < 1)) {
+    alert("Please Enter At least a Word to Create a Cipher Encryption");
+    return;
+  }
+  const shift = 5;
+  let encryptedText = "";
+  const plainWordLength = plaintext.length;
+  for (var i = 0; i < plainWordLength; i++) {
+    const plainCode = plaintext.charCodeAt(i);
+    if (plainCode >= 97 && plainCode <= 122) {
+      encryptedText += String.fromCharCode(((plainCode - 97 - shift + 26) % 26) + 97);
     }
-    var shift = 5;
-    var encryptedtext = "";
-    var plainWordLength = plaintext.length;
-    for (var i = 0; i < plainWordLength; i++) {
-        var plaincode = plaintext.charCodeAt(i);
-        if (plaincode >= 97 && plaincode <= 122) {
-            encryptedtext += String.fromCharCode(((plaincode - 97 - shift + 26) % 26) + 97);
-        }
-        else if (plaincode >= 65 && plaincode <= 90) {
-            encryptedtext += String.fromCharCode(((plaincode - 65 - shift + 26) % 26) + 65);
-        }
-        else encryptedtext += plaintext.charAt(i);
-        document.getElementById("output").value = encryptedtext;
+    else if (plainCode >= 65 && plainCode <= 90) {
+      encryptedText += String.fromCharCode(((plainCode - 65 - shift + 26) % 26) + 65);
     }
-    return encryptedtext;
+    else encryptedText += plaintext.charAt(i);
+    document.getElementById("output").value = encryptedText;
+  }
+  return encryptedText;
 }
 
-function decrypt() {
-    var encryptedtext = document.getElementById('input').value;
-    if ((encryptedtext.length < 1)) {
-        alert("Please Enter Atleast a Word to Create a Cipher Encryption");
-        return;
+decrypt = () => {
+  let decryptedText = "";
+  const shift = 5;
+  const encryptedText = document.getElementById('input').value;
+  if ((encryptedText.length < 1)) {
+    alert("Please Enter At least a Word to Create a Cipher Decryption");
+    return;
+  }
+  const cipherWordLength = encryptedText.length;
+  for (var i = 0; i < cipherWordLength; i++) {
+    const encryptCode = encryptedText.charCodeAt(i);
+    if (encryptCode >= 97 && encryptCode <= 122) {
+      decryptedText += String.fromCharCode(((encryptCode - 97 + shift) % 26) + 97);
     }
-    var shift1 = 5;
-    var decryptedtext = "";
-    var cipherWordLength = encryptedtext.length;
-    for (var i = 0; i < cipherWordLength; i++) {
-        var encryptcode = encryptedtext.charCodeAt(i);
-        if (encryptcode >= 97 && encryptcode <= 122) {
-            decryptedtext += String.fromCharCode(((encryptcode - 97 + shift1) % 26) + 97);
-        }
-        else if (encryptcode >= 65 && encryptcode <= 90) {
-            decryptedtext += String.fromCharCode(((encryptcode - 65 + shift1) % 26) + 65);
-        }
-        else decryptedtext += encryptedtext.charAt(i);
-        document.getElementById("output").value = decryptedtext;
+    else if (encryptCode >= 65 && encryptCode <= 90) {
+      decryptedText += String.fromCharCode(((encryptCode - 65 + shift) % 26) + 65);
     }
-    return decryptedtext;
+    else decryptedText += encryptedText.charAt(i);
+    document.getElementById("output").value = decryptedText;
+  }
+  return decryptedText;
 }
